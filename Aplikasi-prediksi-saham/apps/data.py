597a81138d62f67c78a8882fcca6420ddf1c877b
Muhammad Hugo <80564743+hugomuhammad@users.noreply.github.com>
2021-12-10 07:54:45 +0000

    Add files via upload

--- Aplikasi-prediksi-saham/apps/data.py
+++ Aplikasi-prediksi-saham/apps/data.py
@@ -6,9 +6,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import classification_report
-import tensorflow as tf
-from tensorflow.keras.models import Sequential, save_model, load_model
 from sklearn import datasets
 
 def app():
@@ -50,31 +49,17 @@ def app():
         #split data
         X_train, X_test, y_train, y_test = split(X, y)
 
-        #normalisasi
-        X_train = sc.fit_transform(X_train)
-        X_test  = sc.fit_transform(X_test)
-        y_train = sc.fit_transform(y_train.reshape(-1,1))
-
-        #reshape data agar bisa diinput ke model
-        X_train = np.reshape(X_train,(X_train.shape[0],X_train.shape[1],1))
-        X_test  = np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
 
         return  X_train, X_test, y_train, y_test
 
     #fungsi membbuat model
     def build_model(X_train, y_train):
+    
+        #menggunakan model linear regression
+        model = LinearRegression()
+        model.fit(X_train, y_train)
 
-        lstm_model= tf.keras.Sequential()
-        lstm_model.add(tf.keras.layers.LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1],1)))
-        lstm_model.add(tf.keras.layers.LSTM(units=50))
-        lstm_model.add(tf.keras.layers.Dense(1))
-
-        lstm_model.compile(loss='mean_squared_error',optimizer='adam')
-        lstm_model.fit(X_train, y_train, epochs=200 , batch_size=32,verbose=2)
-
-        #filepath = './saved_model'
-        #save_model(lstm_model, filepath)
-        return lstm_model 
+        return model
 
     global dataframe
     global model
